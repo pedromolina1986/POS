ece61dc1a7ff78b4c176abc308223f6911437a67
phkmolina1986 <pedro.kuchak@gmail.com>
2025-04-02 21:42:57 -0600

    Checkout and Receipt

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,15 +1,22 @@
-let productList = [
-    {
-        id: 1,
-        name: "Milk",
-        price: 1.75
-    },
-    {
-        id: 2,
-        name: "Rice",
-        price: 2.60
-    }
-];
+let productList = [];
+let checkout = [];
+if (!localStorage.getItem("productList")) {
+    productList = [
+        {
+            id: 1,
+            name: "Milk",
+            price: 1.75
+        },
+        {
+            id: 2,
+            name: "Rice",
+            price: 2.60
+        }
+    ];
+    localStorage.setItem("productList",JSON.stringify(productList));
+} else {
+    productList = JSON.parse(localStorage.getItem("productList"));
+};
 
 /*ADMIN SETTINGS PAGE SCRIPTS*/
 let productForm = document.getElementById("productForm");
@@ -19,6 +26,7 @@ let productsBrowse = document.getElementById("productsBrowse");
 let productPriceForm = document.getElementById("productPriceForm");
 let productSelect = document.getElementById("productSelect");
 let productPriceUpdate = document.getElementById("productPriceUpdate");
+let checkoutForm = document.getElementById("checkoutForm");
 
 fillProductTable(productList);
 
@@ -30,12 +38,11 @@ if (window.location.pathname == "/pages/admin.html"){
     
         let newItem = {
             name: productNameInput.value,
-            price: productPriceInput.value
+            price: parseFloat(productPriceInput.value)
         }
         saveProduct(newItem);
         productFormInput.value = "";
-        productPriceInput.value = "";
-        fillSelect();
+        productPriceInput.value = "";        
     });
     
     productPriceForm.addEventListener('submit', function (event) {
@@ -46,16 +53,14 @@ if (window.location.pathname == "/pages/admin.html"){
         if (productSelect.value > 0 ) {
             let updatedItem = {
                 id: productSelect.value,            
-                price: productPriceUpdate.value
-            }
-            console.log(updatedItem);
+                price: parseFloat(productPriceUpdate.value)
+            }            
             let newPrice = document.getElementById("PRODUCTPRICE" + updatedItem.id);
             newPrice.value = parseFloat(updatedItem.price);
             saveProduct(updatedItem);
-            productPriceUpdate.value = 0;
-            fillSelect();
+            productPriceUpdate.value = 0;            
         } else {
-            alert("Pick a product!");
+            alert("Please select a product!");
             productSelect.focus();
         };
     
@@ -131,7 +136,7 @@ function createItemnBrowse(item) {
     inputPrice.type = "number";
     inputPrice.size = 10;
     inputPrice.step = "0.01";
-    inputPrice.value = item.price;
+    inputPrice.value = parseFloat(item.price).toFixed(2);;
     inputPrice.id = "PRODUCTPRICE" + item.id;
     price.appendChild(inputPrice);
 
@@ -192,7 +197,7 @@ function saveProduct(item) {
         let newItem = {
             id: productList.length > 0 ? productList[productList.length - 1].id + 1 : 1,
             name: item.name,
-            price: item.price
+            price: parseFloat(item.price)
         }
         productList.push(newItem);
         createItemnBrowse(newItem);
@@ -206,6 +211,7 @@ function saveProduct(item) {
         }, 2000);
     };
     fillSelect();
+    localStorage.setItem("productList",JSON.stringify(productList));
 
 };
 
@@ -218,5 +224,228 @@ function deleteProduct(id) {
         document.getElementById("PRODUCT" + id).remove();
     };
     fillSelect();
+    localStorage.setItem("productList",JSON.stringify(productList));
 }
-/*ADMIN SETTINGS PAGE SCRIPTS*/
+/*END - ADMIN SETTINGS PAGE SCRIPTS*/
+
+/*CHECKOUT*/
+function keyboardPress(valuePressed) {
+    let unitInput = document.getElementById("productUnit");
+    unitInput.value = valuePressed;
+};
+
+if (window.location.pathname == "/index.html"){
+    checkoutForm.addEventListener('submit', function (event) {
+        
+        // Prevent the default action of sending the form data to the server
+        event.preventDefault();
+        let productSelect = document.getElementById("productSelect");
+        if (productSelect.value > 0) {
+            let item = productList.find(f => f.id == productSelect.value);
+            let unitInput = document.getElementById("productUnit");
+            item.qty = parseInt(unitInput.value);
+            unitInput.value=0;
+            productSelect.value=0;
+            checkout.push(item);
+            
+            //insert item on checkout area
+            let checkoutArea = document.getElementById("checkoutArea");
+            let newCheckout = document.createElement("div");
+            let newCheckoutTitle = document.createElement("h2");
+            newCheckoutTitle.textContent = item.name; 
+            let newCheckoutPrice = document.createElement("span");
+            newCheckoutPrice.textContent = "$" + parseFloat(item.price).toFixed(2);;
+            let newCheckoutQty = document.createElement("span");
+            newCheckoutQty.textContent = "x" + item.qty;
+            let newCheckoutTotal = document.createElement("span");
+            newCheckoutTotal.textContent = "$"+parseFloat(item.price * item.qty).toFixed(2);
+            newCheckout.appendChild(newCheckoutTitle);
+            newCheckout.appendChild(newCheckoutPrice);
+            newCheckout.appendChild(newCheckoutQty);
+            newCheckout.appendChild(newCheckoutTotal);
+            newCheckout.classList.add("checkoutItem");
+            checkoutArea.appendChild(newCheckout);
+
+            //update subtotal
+            let payButton = document.getElementById("payButton");
+            payButton.innerText = "Pay - $" + subTotal();
+
+
+        } else {
+            alert("Please select a product!")
+        }        
+    });
+}
+
+function subTotal() {
+    let total = 0;
+    checkout.map(item => {
+        total += item.price * item.qty;
+    });
+    return parseFloat(total).toFixed(2);
+}
+
+function newTransction() {
+    let checkoutArea = document.getElementById("checkoutArea");    
+    while (receiptArea.lastElementChild) {
+        receiptArea.removeChild(receiptArea.lastElementChild);
+    }; 
+    while (checkoutArea.lastElementChild) {
+        checkoutArea.removeChild(checkoutArea.lastElementChild);
+    };    
+    checkout=[];
+
+     //hiide all div from chechout leaving just the new transaction button    
+     let payButton = document.getElementById("payButton");
+     let keyboard = document.getElementById("keyboard");     
+ 
+     checkoutForm.style.visibility = "visible";
+     payButton.style.visibility = "visible";
+     keyboard.style.visibility = "visible";
+     checkoutArea.style.visibility = "visible";
+}
+
+function payCheckout() {
+    if (subTotal() > 0){        
+        printReceipt();
+        alert("Thank you for your purchase! We hope to see you back soon!");
+        checkout=[];
+    };
+};
+
+function printReceipt() {
+    //hiide all div from chechout leaving just the new transaction button    
+    let payButton = document.getElementById("payButton");
+    let keyboard = document.getElementById("keyboard");
+    let checkoutArea = document.getElementById("checkoutArea");
+
+    checkoutForm.style.visibility = "hidden";
+    payButton.style.visibility = "hidden";
+    keyboard.style.visibility = "hidden";
+    checkoutArea.style.visibility = "hidden";
+    
+    let receiptArea = document.getElementById("receiptArea");
+    
+    /*
+    <table>
+        <tr>
+            <td colspan="4">Date: April 4, 2025</td>
+        </tr>
+        <tr>
+            <td colspan="4">Time: 8:49 PM</td>
+        </tr>
+        <tr>
+            <th>Product</th>
+            <th>$/Unit</th>
+            <th>Unit(s)</th>
+            <th>Price</th>
+        </tr>
+        <tr>
+            <td>Milk</td>
+            <td>1.75</td>
+            <td>2</td>
+            <td>3.50</td>
+        </tr>
+        <tr>
+            <td colspan="4">Total Price: 3.50</td>
+        </tr>
+        <tr>
+            <td colspan="4">Taxes (5%): 0.18</td>
+        </tr>
+        <tr>
+            <td colspan="4">Total Price: 3.68</td>
+        </tr>
+    </table>
+    */
+    let table = document.createElement("table");
+    
+    let trDate = document.createElement("tr");    
+    let tdDate = document.createElement("td");
+    const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' };
+    const currentDate = new Date().toLocaleDateString('en-US', optionsDate);
+    tdDate.textContent = "Date: " + currentDate;
+    tdDate.colSpan = 4;
+    trDate.appendChild(tdDate);
+    table.appendChild(trDate);
+
+    let trTime = document.createElement("tr");
+    let tdTime = document.createElement("td");
+    const now = new Date();
+    const options = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const currentTime = new Intl.DateTimeFormat('en-US', options).format(now);    
+    tdTime.textContent = "Date: " + currentTime;
+    tdTime.colSpan = 4;
+    trTime.appendChild(tdTime);
+    table.appendChild(trTime);    
+
+    let trProducts;
+    let tdProductsName;
+    let tdProductsPrice;
+    let tdProductsUnits;
+    let tdProductsTotal;
+
+    trProducts = document.createElement("tr");
+    tdProductsName = document.createElement("th");
+    tdProductsPrice = document.createElement("th");
+    tdProductsUnits = document.createElement("th");
+    tdProductsTotal = document.createElement("th");
+
+    tdProductsName.textContent = "Product";
+    tdProductsPrice.textContent = "$/Unit";
+    tdProductsUnits.textContent = "Unit(s)";
+    tdProductsTotal.textContent = "Price";
+
+    trProducts.appendChild(tdProductsName);
+    trProducts.appendChild(tdProductsPrice);
+    trProducts.appendChild(tdProductsUnits);
+    trProducts.appendChild(tdProductsTotal);
+    table.appendChild(trProducts);
+
+    let subTotal = 0;
+    checkout.map(product => {
+        trProducts = document.createElement("tr");
+        tdProductsName = document.createElement("td");
+        tdProductsPrice = document.createElement("td");
+        tdProductsUnits = document.createElement("td");
+        tdProductsTotal = document.createElement("td");
+
+        tdProductsName.textContent = product.name;
+        tdProductsPrice.textContent = parseFloat(product.price).toFixed(2);
+        tdProductsUnits.textContent = parseInt(product.qty);
+        tdProductsTotal.textContent = parseFloat(product.price*product.qty).toFixed(2);
+
+        subTotal += parseFloat(product.price*product.qty);
+
+        trProducts.appendChild(tdProductsName);
+        trProducts.appendChild(tdProductsPrice);
+        trProducts.appendChild(tdProductsUnits);
+        trProducts.appendChild(tdProductsTotal);
+
+        table.appendChild(trProducts);
+    })    
+
+    let trSubtotal = document.createElement("tr");
+    let tdSubtotal = document.createElement("td");
+    tdSubtotal.textContent = "Total Price: " + parseFloat(subTotal).toFixed(2);
+    tdSubtotal.colSpan = 4;
+    trSubtotal.appendChild(tdSubtotal);
+    table.appendChild(trSubtotal);
+
+    let trTaxes = document.createElement("tr");
+    let tdTaxes = document.createElement("td");
+    let taxes = parseFloat(subTotal*0.05).toFixed(2);
+    tdTaxes.textContent = "Taxes: " + parseFloat(subTotal*0.05).toFixed(2);
+    tdTaxes.colSpan = 4;
+    trTaxes.appendChild(tdTaxes);
+    table.appendChild(trTaxes);
+
+    let trTotal = document.createElement("tr");
+    let tdTotal = document.createElement("td");
+    tdTotal.textContent = "Amount Due: " + parseFloat(subTotal+taxes).toFixed(2);
+    tdTotal.colSpan = 4;
+    trTotal.appendChild(tdTotal);
+    table.appendChild(trTotal);
+
+    receiptArea.appendChild(table);
+}
+/*END - CHECKOUT*/
